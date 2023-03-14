d140e6cb75a0e2176e3ff0d9bd32696e8fb49dde
MCHR-DIGITAL <127359725+MCHR-DIGITAL@users.noreply.github.com>
2023-03-14 00:19:41 -0300

    Add files via upload

---
+++ assets/js/vsl.js
@@ -0,0 +1,7 @@
+// <!-- START VSL VÍDEO -->
+var s = document.createElement('script');
+(s.src =
+  'https://scripts.converteai.net/4d9fce37-6aa5-4452-a12c-f819cbd26f5d/players/6408e0832b2cc8000b5157ad/player.js'),
+  (s.async = !0),
+  document.head.appendChild(s);
+// <!-- End VSL VÍDEO -->
