e43dd91f986b4bab06ad2aff92122595306cea7f
MCHR-DIGITAL <127359725+MCHR-DIGITAL@users.noreply.github.com>
2023-03-28 01:09:46 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,4 @@ const actionButtons = document.querySelector('.buttons-action');
 const timeOutButtons = setTimeout(() => {
   actionButtons.style.display = 'flex';
   clearTimeout(timeOutButtons);
-}, 1800 * 1000); // Faz o actionButtons ser mostrado após 15 segundos
+}, 20 * 1000); // Faz o actionButtons ser mostrado após 15 segundos
