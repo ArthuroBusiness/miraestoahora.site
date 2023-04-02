4d5886698bcf875a5bb612acd4d9885356657f4d
MCHR-DIGITAL <127359725+MCHR-DIGITAL@users.noreply.github.com>
2023-04-01 22:51:10 -0300

    Update vsl.js

--- assets/js/vsl.js
+++ assets/js/vsl.js
@@ -1,3 +1,3 @@
 // <!-- START VSL VÍDEO -->
-var s=document.createElement("script");s.src="https://scripts.converteai.net/63cae839-eb70-4631-9b5e-ca43f55e8c0a/players/6425a2201e83d30009749a99/player.js",s.async=!0,document.head.appendChild(s);
+var s=document.createElement("script");s.src="https://scripts.converteai.net/63cae839-eb70-4631-9b5e-ca43f55e8c0a/players/6428da9e49342600085e92e3/player.js",s.async=!0,document.head.appendChild(s);
 // <!-- End VSL VÍDEO -->
