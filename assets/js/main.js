118fc87549e2dcb0742b1fa2ea9b0e8cdd1f288d
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-07-16 22:20:08 -0300

    Refactor layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 const actionButtons = document.querySelector('.buttons-action');
+const pricesSection = document.querySelector('.prices');
 
 const timeOutButtons = setTimeout(() => {
   actionButtons.style.display = 'flex';
+  pricesSection.style.display = 'flex';
   clearTimeout(timeOutButtons);
 }, 3 * 1000); // Faz o actionButtons ser mostrado após 15 segundos
