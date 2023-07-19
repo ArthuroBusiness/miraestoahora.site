3c1c9a9aad486def26c7f1c908b12bfe2944bd48
ArthuroBusiness <139590972+ArthuroBusiness@users.noreply.github.com>
2023-07-19 00:16:40 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,4 @@ const timeOutButtons = setTimeout(() => {
   actionButtons.style.display = 'flex';
   pricesSection.style.display = 'flex';
   clearTimeout(timeOutButtons);
-}, 3 * 1000); // Faz o actionButtons ser mostrado após 15 segundos
+}, 989 * 1000); // Faz o actionButtons ser mostrado após 15 segundos
