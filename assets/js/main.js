050e0453411d991ccb1909602193e29c4cc8fb6f
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-07-25 23:40:37 -0300

    update call to action buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,19 @@
-const secondsToButtonsAppear = 3
-const milliseconds = secondsToButtonsAppear * 1000
+const secondsToButtonsAppear = 1200
 
 const actionButtons = document.querySelector('.buttons-action');
 const pricesSection = document.querySelector('.prices');
 
-const timeOutButtons = setTimeout(() => {
-  actionButtons.style.display = 'flex';
-  pricesSection.style.display = 'flex';
-  clearTimeout(timeOutButtons);
-}, milliseconds);
+const changeActualTime = setInterval(() => {
+  const updatedTime = document.querySelector('span.smartplayer-ptime')
+  const [minute, second] = updatedTime.innerHTML.split(':')
+
+  const passedSeconds = +minute * 60 + +second
+
+  if (passedSeconds >= secondsToButtonsAppear) {
+    actionButtons.style.display = 'flex';
+    pricesSection.style.display = 'flex';
+  } else {
+    actionButtons.style.display = 'none';
+    pricesSection.style.display = 'none';
+  }
+}, 1 * 1000)
