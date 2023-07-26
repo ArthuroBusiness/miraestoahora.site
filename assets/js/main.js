8c282e07f7fb6647a66e3afa2e38d4c3d70fc96e
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-07-25 22:13:23 -0300

    change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+const secondsToButtonsAppear = 3
+const milliseconds = secondsToButtonsAppear * 1000
+
 const actionButtons = document.querySelector('.buttons-action');
 const pricesSection = document.querySelector('.prices');
 
@@ -5,4 +8,4 @@ const timeOutButtons = setTimeout(() => {
   actionButtons.style.display = 'flex';
   pricesSection.style.display = 'flex';
   clearTimeout(timeOutButtons);
-}, 3 * 1000); // Faz o actionButtons ser mostrado após 15 segundos
+}, milliseconds);
