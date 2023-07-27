fb40bf1d01201f9ced53be440145cccdb3e2b28e
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-07-26 23:02:58 -0300

    change time script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,42 @@
-const secondsToButtonsAppear = 1200
+const secondsToButtonsAppear = 10
 
 const actionButtons = document.querySelector('.buttons-action');
 const pricesSection = document.querySelector('.prices');
 
-const changeActualTime = setInterval(() => {
-  const updatedTime = document.querySelector('span.smartplayer-ptime')
-  const [minute, second] = updatedTime.innerHTML.split(':')
+// const changeActualTime = setInterval(() => {
+//   const updatedTime = document.querySelector('span.smartplayer-ptime')
+//   if (!updatedTime) return
+//   const [minute, second] = updatedTime.innerHTML.split(':')
 
-  const passedSeconds = +minute * 60 + +second
+//   const passedSeconds = +minute * 60 + +second
 
-  if (passedSeconds >= secondsToButtonsAppear) {
-    actionButtons.style.display = 'flex';
-    pricesSection.style.display = 'flex';
-  } else {
-    actionButtons.style.display = 'none';
-    pricesSection.style.display = 'none';
-  }
+//   if (passedSeconds >= secondsToButtonsAppear) {
+//     actionButtons.style.display = 'flex';
+//     pricesSection.style.display = 'flex';
+//   } else {
+//     actionButtons.style.display = 'none';
+//     pricesSection.style.display = 'none';
+//   }
+// }, 1 * 1000)
+
+
+const getVideoTime = setInterval(() => {
+  const video = document.querySelector('video')
+  if (!video) return
+  // const [minute, second] = updatedTime.innerHTML.split(':')
+
+  video.addEventListener('timeupdate', () => {
+    const passedSeconds = video.currentTime
+    
+    if (passedSeconds >= secondsToButtonsAppear) {
+      actionButtons.style.display = 'flex';
+      pricesSection.style.display = 'flex';
+    } else {
+      actionButtons.style.display = 'none';
+      pricesSection.style.display = 'none';
+    }
+  });
+
+
+  clearInterval(getVideoTime)
 }, 1 * 1000)
